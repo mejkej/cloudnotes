22aac3eb1f9ca8454fa686ffd7d657ccf3daf046
mejkej <krus3@icloud.com>
2023-04-29 21:02:04 +0000

    Styling of entry page nearly done

---
+++ static/js/entry.js
@@ -0,0 +1,16 @@
+{% load static %}
+
+const signInBtn = document.getElementById('signinbtn')
+const signUpBtn = document.getElementById('signupbtn')
+const signInForm = document.getElementById('signinform')
+const signInBtn = document.getElementById('signupform')
+const signInBtn = document.getElementById('signinbtn')
+const signInBtn = document.getElementById('signinbtn')
+
+id="signinbtn"
+id="signinform"
+id="submitsigninbtn"
+id="signupbtn"
+id="signupform"
+id="submitsignupbtn"
+class="hidden"
