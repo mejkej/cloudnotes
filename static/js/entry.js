21dcf8b232e527dfbe7dba69d79db0e9e21975e4
mejkej <krus3@icloud.com>
2023-04-29 21:19:33 +0000

    cashed the dom

--- static/js/entry.js
+++ static/js/entry.js
@@ -1,16 +1,11 @@
 {% load static %}
 
-const signInBtn = document.getElementById('signinbtn')
-const signUpBtn = document.getElementById('signupbtn')
-const signInForm = document.getElementById('signinform')
-const signInBtn = document.getElementById('signupform')
-const signInBtn = document.getElementById('signinbtn')
-const signInBtn = document.getElementById('signinbtn')
+const signInBtn = document.getElementById('signinbtn');
+const signUpBtn = document.getElementById('signupbtn');
+const signInForm = document.getElementById('signinform');
+const signUpForm = document.getElementById('signupform');
+const signInSubmit = document.getElementById('submitsigninbtn');
+const signUpSubmit = document.getElementById('submitsignupbtn');
 
-id="signinbtn"
-id="signinform"
-id="submitsigninbtn"
-id="signupbtn"
-id="signupform"
-id="submitsignupbtn"
-class="hidden"
+
+
