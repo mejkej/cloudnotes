6952d09bd007c8dd5441e7011fd70e317eb55674
mejkej <krus3@icloud.com>
2023-04-30 12:09:42 +0000

    added some html and js now having issues access temps

--- static/js/entry.js
+++ static/js/entry.js
@@ -1,11 +1,17 @@
-{% load static %}
-
 const signInBtn = document.getElementById('signinbtn');
 const signUpBtn = document.getElementById('signupbtn');
 const signInForm = document.getElementById('signinform');
 const signUpForm = document.getElementById('signupform');
 const signInSubmit = document.getElementById('submitsigninbtn');
 const signUpSubmit = document.getElementById('submitsignupbtn');
+
+signInBtn.addEventListener('click', () => {
+    signInForm.classList.remove('hidden');
+})
+
+signUpBtn.addEventListener('click', () => {
+    signUpForm.classList.remove('hidden');
+})
 
 
 
