b200a6317eb3d9b8d14366636a0b3710fb69edd0
mejkej <krus3@icloud.com>
2023-05-10 16:50:13 +0000

    updated the signin and signup html files

--- entry_app/views.py
+++ entry_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import SignUpForm, SignInForm
 
 
 def signin_view(request):
-    return render(request, 'entry_app/signin.html')
+    return render(request, 'signin.html')
 
